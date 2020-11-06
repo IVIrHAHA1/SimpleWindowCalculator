8b298b5e94f47fce23e24a90c375ec96e496bedb
HAHA <jperez5385@gmail.com>
2020-11-05 21:29:22 -0800

    added factor count in window pallet

--- lib/widgets/WindowPallet.dart
+++ lib/widgets/WindowPallet.dart
@@ -19,41 +19,80 @@ class WindowPallet extends StatelessWidget {
             thickness: 2,
             height: 4,
           ),
-          Container(
-            alignment: Alignment.topCenter,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Column(
-                children: windowList.map((e) {
-                  return e.getPicture() != null
-                      ? Column(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Stack(
-                              children: [
-                                Container(
-                                  width: double.infinity,
-                                  child: e.getPicture(),
-                                  height:
-                                      MediaQuery.of(context).size.height / 6,
+          Flexible(
+            fit: FlexFit.tight,
+            child: Container(
+              alignment: Alignment.topCenter,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: windowList.map((window) {
+                    return window.getPicture() != null
+                        ? Column(
+                            children: [
+                              ListTile(
+                                leading: Text(
+                                  '${Format.format(window.getCount())}',
+                                  style: Theme.of(context).textTheme.headline5,
                                 ),
-                                Positioned(
-                                  child: Text('${Format.format(e.getCount())}'),
-                                  top: 10,
-                                  right: 10,
+                                trailing: Text(
+                                    '\$${Format.format(window.grandTotal())}'),
+                              ),
+                              Container(
+                                width: double.infinity,
+                                child: Stack(
+                                  children: [
+                                    // Sets the width of the Column
+                                    Container(
+                                      child: window.getPicture(),
+                                      height:
+                                          MediaQuery.of(context).size.height /
+                                              6,
+                                    ),
+                                    Positioned(
+                                      child: Container(
+                                        height:
+                                            MediaQuery.of(context).size.height /
+                                                6,
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceEvenly,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.end,
+                                          children: window.factorList.entries
+                                              .map((entry) {
+                                            return Container(
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.end,
+                                                children: [
+                                                  Text(
+                                                      '${Format.format(entry.value.getCount())}'),
+                                                  entry.value.getImage(),
+                                                ],
+                                              ),
+                                              height: 20,
+                                            );
+                                          }).toList(),
+                                        ),
+                                      ),
+                                      top: 10,
+                                      right: 10,
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
-                            Divider(
-                              thickness: 2,
-                              height: 2,
-                              endIndent: 8,
-                              indent: 8,
-                            )
-                          ],
-                        )
-                      : Icon(Icons.explicit);
-                }).toList(),
+                              ),
+                              Divider(
+                                thickness: 2,
+                                height: 5,
+                                endIndent: 8,
+                                indent: 8,
+                              ),
+                            ],
+                          )
+                        : Icon(Icons.explicit);
+                  }).toList(),
+                ),
               ),
             ),
           ),
